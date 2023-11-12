ec3ffbb86bc5a0d3a1528682e0cb783a368b6184
iloozealot <iloozealot@gmail.com>
2023-11-11 22:08:52 -0600

    Add project files.

---
+++ LearningIntVariables/Program.cs
@@ -0,0 +1,20 @@
+﻿// Whole numbers only 
+//Yes 2,4, 18, -9
+//No 1.2, -5.7
+
+int age = 0;
+
+age = 43;
+
+int ageInTenYears = age + 10;
+
+//Can be two billion numbers long +/-
+//signed Int32
+//unsigned - four billion
+//bit - 0 or 1
+//byte - 8 bits - 00000000
+
+Console.WriteLine(age);
+Console.WriteLine(ageInTenYears);
+//Console.WriteLine(age / 2);don't divide int it cuts off decimals because whole numbers only
+
